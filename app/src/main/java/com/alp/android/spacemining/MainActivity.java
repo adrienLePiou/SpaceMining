636888d012d55e98317693c8dd2276b84ddcd8f0
adrienlepiou <adrien.lecoarer@gmail.com>
2015-09-27 15:26:46 +0200

    POP Menu for the hero enhancement. May need to find something else than a popup menu.

--- app/src/main/java/com/alp/android/spacemining/MainActivity.java
+++ app/src/main/java/com/alp/android/spacemining/MainActivity.java
@@ -6,14 +6,34 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+
 
 import java.util.Random;
 
@@ -23,6 +43,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button popUpButton = (Button) findViewById(R.id.open);
+        popUpButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                if (p != null)
+                    showPopup(MainActivity.this, p);
+            }
+        });
     }
 
     @Override
@@ -47,6 +80,81 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    private Point p;
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        int[] location = new int[2];
+        Button button = (Button) findViewById(R.id.open);
+
+        button.getLocationOnScreen(location);
+        // Initialize the Point with x, and y positions
+        p = new Point();
+        p.x = location[0];
+        p.y = location[1];
+
+
+
+    }
+
+
+
+    public void showPopup(final Activity context, Point p) {
+
+        Rect rectgle= new Rect();
+        Window window= getWindow();
+        window.getDecorView().getWindowVisibleDisplayFrame(rectgle);
+        int StatusBarHeight= rectgle.top;
+        int contentViewTop=
+                window.findViewById(Window.ID_ANDROID_CONTENT).getTop();
+        int TitleBarHeight= contentViewTop - StatusBarHeight;
+        Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        int popupWidth = display.getWidth();
+        int popupHeight = (display.getHeight()-TitleBarHeight);
+
+        // Inflate the popup_layout.xml
+        RelativeLayout viewGroup = (RelativeLayout) context
+                .findViewById(R.id.LinearCosmonautePopup);
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.cosmonaute_popup, viewGroup);
+
+
+        // Creating the PopupWindow
+        final PopupWindow popup = new PopupWindow(context);
+        popup.setContentView(layout);
+        popup.setWidth(popupWidth);
+        popup.setHeight(popupHeight);
+        popup.setFocusable(true);
+        popup.setAnimationStyle(R.style.PopupWindowAnimation);
+        ((TextView)popup.getContentView().findViewById(R.id.pu_cd_cost)).setText(String.valueOf(heroCDCost));
+        ((TextView)popup.getContentView().findViewById(R.id.totalCDTxt)).setText(String.valueOf(clickDmg));
+
+
+        // Some offset to align the popup a bit to the right, and a bit down,
+        // relative to button's position.
+
+        int OFFSET_X = 0;
+        int OFFSET_Y = 0;
+        // Clear the default translucent background
+        popup.setBackgroundDrawable(new BitmapDrawable());
+        // Displaying the popup at the specified location, + offsets.
+        popup.showAtLocation(layout, Gravity.NO_GRAVITY, p.x + OFFSET_X, p.y
+                + OFFSET_Y);
+
+        // Getting a reference to Close button, and close the popup when
+        // clicked.
+        Button close = (Button) layout.findViewById(R.id.close);
+        close.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popup.dismiss();
+            }
+        });
+
+
+    }
+
     public void displayCurrentLvl(int lvl){
         TextView currentLvlText = (TextView) findViewById(R.id.currentLvlText);
         currentLvlText.setText(String.valueOf(lvl));
@@ -62,11 +170,6 @@ public class MainActivity extends Activity {
         monsterNameTxt.setText("" + monsterName);
     }
 
-    public void displayClickDmg(int clickDmg){
-        TextView totalCDTxt = (TextView) findViewById(R.id.totalCDTxt);
-        totalCDTxt.setText(String.valueOf("Click Damage: " + clickDmg));
-    }
-
     public void displayAsteroidsKilled(int astKilled){
         TextView totalMKTxt = (TextView) findViewById(R.id.totalMKTxt);
         totalMKTxt.setText(String.valueOf(astKilled + "/10"));
@@ -78,15 +181,25 @@ public class MainActivity extends Activity {
     }
 
     public void displayNextPuCDCost(int heroCDCost){
-        TextView powerUpCost = (TextView) findViewById(R.id.pu_cd_cost);
-        powerUpCost.setText(String.valueOf(heroCDCost + " Golds"));
+
+        LayoutInflater layoutInflater =
+                (LayoutInflater)MainActivity.this
+                        .getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = layoutInflater.inflate(R.layout.cosmonaute_popup, null);
+        PopupWindow popupWindow = new PopupWindow(getBaseContext());
+
+
+        ((TextView)popupWindow.getContentView().findViewById(R.id.pu_cd_cost)).setText(String.valueOf(heroCDCost));
+       // TextView heroCDCostTxt = (TextView) popupView.getContentView().findViewById(R.id.pu_cd_cost);
+        //heroCDCostTxt.setText(String.valueOf(heroCDCost));
+
+
     }
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     public void displayAsteroidStyle(String style){
         ImageView image = (ImageView) findViewById(R.id.asteroidView);
-        String myDrawableName = style;
-        int resId = getResources().getIdentifier(myDrawableName, "drawable", getPackageName());
+        int resId = getResources().getIdentifier(style, "drawable", getPackageName());
         Bitmap bMap = BitmapFactory.decodeResource(getResources(), resId);
         image.setImageBitmap(bMap);
     }
@@ -98,7 +211,7 @@ public class MainActivity extends Activity {
     int lvl = 1;
     String AsteroidStyle = Asteroid.getAsteroidStyle();
     int astKilled = 0;
-    int totalCrystal = 0;
+    int totalCrystal = 10;
     int clickDmg = cosmonaute.getClickDamage();
     int heroCDCost = 10;
     int heroLvl = cosmonaute.getCosmonauteLvl();
@@ -119,7 +232,7 @@ public class MainActivity extends Activity {
     }
 
     /* Increase Click Damage */
-    public boolean setClickDmg(){
+    public void setClickDmg(View popup){
         if(totalCrystal - heroCDCost >= 0){
             cosmonaute.setClickDamage(1);
             clickDmg = cosmonaute.getClickDamage();
@@ -129,75 +242,27 @@ public class MainActivity extends Activity {
             heroCDCost = (int) Math.floor(heroCDCost * Math.pow(1.07, heroLvl));
             displayTotalCrystal(totalCrystal);
             displayNextPuCDCost(heroCDCost);
-            displayClickDmg(clickDmg);
+            //displayClickDmg(clickDmg);
+
+
 
             Toast.makeText(
                     MainActivity.this,
-                    "Your Cosmonaute is Lvl" + cosmonaute.getCosmonauteLvl(),
+                    "Your Cosmonaute is now level " + cosmonaute.getCosmonauteLvl(),
                     Toast.LENGTH_SHORT
             ).show();
 
-            return true;
+
         } else {
             Toast.makeText(
                     MainActivity.this,
                     "You don't have enough crystals",
                     Toast.LENGTH_SHORT
             ).show();
-            return false;
+
         }
 
     }
-
-
-
-    //Create Cosmonaute Pop Up Menu
-    public void CreatePopupMenu(View v) {
-
-        PopupMenu mypopupmenu = new PopupMenu(this, v);
-        MenuInflater inflater = mypopupmenu.getMenuInflater();
-        inflater.inflate(R.menu.cosmonaute_menu, mypopupmenu.getMenu());
-        mypopupmenu.show();
-
-        //registering popup with OnMenuItemClickListener
-        mypopupmenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-            public boolean onMenuItemClick(MenuItem item) {
-                switch (item.getItemId()) {
-
-                    case R.id.option1:
-                        setClickDmg();
-
-                        return true;
-
-                    case R.id.option2:
-
-                        Toast.makeText(
-                                MainActivity.this,
-                                "Option 2 Clicked",
-                                Toast.LENGTH_SHORT
-                        ).show();
-
-                        return true;
-
-                    default:
-
-                        return true;
-
-                }
-
-                /*Toast.makeText(
-                        MainActivity.this,
-                        "Click Damage " + item.getTitle() + " " + item.getItemId(),
-                        Toast.LENGTH_SHORT
-                ).show();*/
-
-
-            }
-
-            });
-
-    }
-
 
     /* What happens when you click*/
     public void hittingAsteroid(View view){
@@ -241,6 +306,7 @@ public class MainActivity extends Activity {
             }
         }
     }
+
 
 
 
